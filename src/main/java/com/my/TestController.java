4cd5b0dfea64514403dd4d63ee7eb7db8e972ac9
Tuanzi <1062635336@qq.com>
2018-05-12 21:56:07 +0800

    add TestController

--- src/main/java/com/my/TestController.java
+++ src/main/java/com/my/TestController.java
@@ -1,7 +1,21 @@
 package com.my;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by Flash on 2018/5/12.
  */
+@Controller
+/**
+ * test
+ */
 public class TestController {
+    @RequestMapping("/test")
+    @ResponseBody
+    public String test(){
+        return "test";
+    }
 }
+
