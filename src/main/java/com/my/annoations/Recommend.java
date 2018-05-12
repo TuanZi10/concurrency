e9d4097ea3695bc24c4cf5c54640aac62734654f
Tuanzi <1062635336@qq.com>
2018-05-12 19:44:16 +0800

    add annoations

---
+++ src/main/java/com/my/annoations/Recommend.java
@@ -0,0 +1,16 @@
+package com.my.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by Flash on 2018/5/12.
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)//注解存在的范围
+public @interface Recommend {//标注线程安全的类
+    String value() default "";
+}
---
+++ src/main/java/com/my/annoations/ThreadSafe.java
@@ -0,0 +1,7 @@
+package com.my.annoations;
+
+/**
+ * Created by Flash on 2018/5/12.
+ */
+public @interface ThreadSafe {
+}
